a75512ecf85396f3526705e84e6d2963cbffbb6d
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2019-04-26 15:53:44 +0700

    Update buble sort algo

--- ruby/bubble_sort.rb
+++ ruby/bubble_sort.rb
@@ -1,10 +1,8 @@
 def bubble_sort(arr)
 	((arr.length) - 1).times do |i|
-		swap = false
 		((arr.length) - 1 - i).times do |j|
 			if(arr[j] > arr[j+1])
 				arr[j],arr[j+1] = arr[j+1],arr[j]
-				swap = true
 			end
 		end
 	end
@@ -13,11 +11,9 @@ end
 
 def bubble_sort_by(arr)
 	((arr.length) - 1).times do |i|
-		swap = false
 		((arr.length) - 1 - i).times do |j|
 			if yield(arr[j],arr[j+1]) < 0
 				arr[j],arr[j+1] = arr[j+1],arr[j]
-				swap = true
 			end
 		end
 	    
@@ -26,5 +22,5 @@ def bubble_sort_by(arr)
 	    p arr
 end
 
-p bubble_sort([1,2,4,3,6,5])
+p bubble_sort([1,2,4,3,6,5,10,7,9,8])
 p bubble_sort_by(["hi","hello","hey"]) { |left, right| right.length - left.length }
